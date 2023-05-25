36be8f5a97cb69de6bc11c6a4c9ddb62befad182
jiyoon0701 <dlwldbs147@gmail.com>
2023-05-25 15:32:18 +0900

    multi module setting
    초기 설정

---
+++ oauth2/resourcesServer/src/main/java/com/example/resourcesserver/ResourcesServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.resourcesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourcesServerApplication.class, args);
+    }
+
+}
