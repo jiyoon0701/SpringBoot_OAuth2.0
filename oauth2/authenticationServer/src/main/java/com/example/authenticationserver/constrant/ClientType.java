59baea66832c989e276515838ac9f3bfb96d95ec
jiyoon0701 <dlwldbs147@gmail.com>
2023-05-26 17:40:01 +0900

    java Util 및  Enum Type 추가

---
+++ oauth2/authenticationServer/src/main/java/com/example/authenticationserver/constrant/ClientType.java
@@ -0,0 +1,10 @@
+package com.example.authenticationserver.constrant;
+
+/**
+ * Client Application Type
+ * @author yun-yeoseong
+ *
+ */
+public enum ClientType {
+	PUBLIC,CONFIDENTIAL
+}
---
+++ oauth2/authenticationServer/src/main/java/com/example/authenticationserver/constrant/UserRole.java
@@ -0,0 +1,6 @@
+package com.example.authenticationserver.constrant;
+
+public enum UserRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
