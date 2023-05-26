5f5e6c6d79979ac5fef0201b18f8c780f47d11b6
jiyoon0701 <dlwldbs147@gmail.com>
2023-05-26 17:43:37 +0900

    OAuth2 members, clients controller & repository & service
    - 플랫폼 가입 및 로그인
    - Provider (clientId, secretId) 생성 및 삭제

---
+++ oauth2/authenticationServer/src/main/java/com/example/authenticationserver/controller/membersController.java
@@ -0,0 +1,50 @@
+package com.example.authenticationserver.controller;
+
+import com.example.authenticationserver.constrant.UserRole;
+import com.example.authenticationserver.entity.ResourceOwner;
+import com.example.authenticationserver.repository.ResourceOwnerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/members")
+@Slf4j
+public class membersController {
+    @Autowired
+    private ResourceOwnerRepository repository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+
+    @PostMapping("register")
+    public String registerPage(ResourceOwner user) {
+        user.setUsername(user.getUsername());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(UserRole.ROLE_USER);
+
+        ResourceOwner owner = repository.findByUseremail(user.getUsername());
+        if(ObjectUtils.isEmpty(owner)){
+            repository.save(user);
+        }
+
+        return "login";
+
+    }
+
+}
---
+++ oauth2/authenticationServer/src/main/java/com/example/authenticationserver/service/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.authenticationserver.service;
+
+import com.example.authenticationserver.entity.ResourceOwner;
+import com.example.authenticationserver.repository.ResourceOwnerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+@Slf4j
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired private ResourceOwnerRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        log.info("UserDetailsServiceImpl.loadUserByUsername :::: {}",username);
+
+        ResourceOwner user = repository.findByUseremail(username);
+
+        if(ObjectUtils.isEmpty(user)){
+            throw new UsernameNotFoundException("Invalid resource owner, please check resource owner info !");
+        }
+        user.setAuthorities(AuthorityUtils.createAuthorityList(String.valueOf(user.getRole())));
+
+        return user;
+    }
+}
