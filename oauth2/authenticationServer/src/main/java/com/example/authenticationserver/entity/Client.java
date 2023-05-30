8fa9ee918ba8c98a376f15309a6fb6eacfc23957
jiyoon0701 <dlwldbs147@gmail.com>
2023-05-30 17:38:22 +0900

    feat : [Register] resource owner & Update filed name
    - 인증서버 구현 완료

--- oauth2/authenticationServer/src/main/java/com/example/authenticationserver/entity/Client.java
+++ oauth2/authenticationServer/src/main/java/com/example/authenticationserver/entity/Client.java
@@ -11,7 +11,7 @@ import org.springframework.security.oauth2.provider.client.BaseClientDetails;
  */
 public class Client extends BaseClientDetails{
 
-	private static final long serialVersionUID = 5840531070411146325L;
+	private static final long serialVersionUID = 5840531070411146325L; // 직렬화 버전의 고유값
 	
 	@Getter
 	private ClientType clientType;
